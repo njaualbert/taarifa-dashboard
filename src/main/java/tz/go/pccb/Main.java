1b249d3a1a1e1983223b78f9dc10682cff21751c
ALBERT NJAU <87620450+njaualbert@users.noreply.github.com>
2022-12-16 09:57:36 +0300

    Added spring dependencies

--- src/main/java/tz/go/pccb/Main.java
+++ src/main/java/tz/go/pccb/Main.java
@@ -1,5 +1,13 @@
-package tz.go.pccb;public class Main {
+package tz.go.pccb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(Main.class, args);
     }
 }
